3fd0fd27714d8728507b8f691001399b628e903a
Lami14 <123112707+Lami14@users.noreply.github.com>
2026-03-05 09:41:05 +0200

    Add room creation and retrieval endpoints

---
+++ connectly-api/routes/roomRoutes.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const router = express.Router();
+const { createRoom, getRooms } = require("../controllers/roomController");
+const protect = require("../middleware/authMiddleware");
+
+router.post("/", protect, createRoom);
+router.get("/", protect, getRooms);
+
+module.exports = router;
