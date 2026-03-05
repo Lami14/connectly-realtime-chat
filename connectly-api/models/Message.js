53ad3c14e8f7411269c3fdb956418fb1c54fe918
Lami14 <123112707+Lami14@users.noreply.github.com>
2026-03-05 09:29:07 +0200

    Create User, Room, and Message MongoDB models

---
+++ connectly-api/models/Message.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+const messageSchema = new mongoose.Schema(
+  {
+    room: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Room",
+    },
+    sender: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Message", messageSchema);
