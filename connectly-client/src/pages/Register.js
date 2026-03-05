4ebadb4858f1414897db91a32df0c4c3b2a15093
Lami14 <123112707+Lami14@users.noreply.github.com>
2026-03-05 10:01:11 +0200

    Set up scalable frontend folder structure

---
+++ connectly-client/src/pages/Register.js
@@ -0,0 +1,50 @@
+import { useState, useContext } from "react";
+import axios from "../api/axios";
+import { AuthContext } from "../context/AuthContext";
+import { useNavigate } from "react-router-dom";
+
+const Register = () => {
+  const [form, setForm] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+
+  const { setUser } = useContext(AuthContext);
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const { data } = await axios.post("/auth/register", form);
+    setUser(data);
+    navigate("/chat");
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <h2>Register</h2>
+      <input
+        placeholder="Username"
+        onChange={(e) =>
+          setForm({ ...form, username: e.target.value })
+        }
+      />
+      <input
+        placeholder="Email"
+        onChange={(e) =>
+          setForm({ ...form, email: e.target.value })
+        }
+      />
+      <input
+        type="password"
+        placeholder="Password"
+        onChange={(e) =>
+          setForm({ ...form, password: e.target.value })
+        }
+      />
+      <button type="submit">Register</button>
+    </form>
+  );
+};
+
+export default Register;
